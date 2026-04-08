729008c2a2cb45d3241be1644622d906525f63e2
Naveen K Muppala <muppala.devops@gmail.com>
2026-04-08 13:41:54 +0530

    Updated instance type to t3.large

--- ec2-terraform/variables.tf
+++ ec2-terraform/variables.tf
@@ -25,7 +25,7 @@ variable "project_name" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  default     = "t3.large"
 }
 
 variable "vpc_id" {
