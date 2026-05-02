0519584c9489dd24a7fb536781cd3b097fc0be9e
Naveen K Muppala <muppala.devops@gmail.com>
2026-05-03 02:16:29 +0530

    Updated subnets naming convention

--- aws-terraform-infra/variables.tf
+++ aws-terraform-infra/variables.tf
@@ -3,3 +3,9 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "ami_name_filter" {
+  description = "AMI name filter for Amazon Linux 2"
+  type        = string
+  default     = "amzn2-ami-hvm-*-x86_64-gp2"
+}
