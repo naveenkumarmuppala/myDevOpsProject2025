02908129b56176f6c10484ce3590c607bc914d99
Naveen K Muppala <muppala.devops@gmail.com>
2026-05-04 00:38:47 +0530

    Updated modules path

--- aws-terraform-infra/envs/dev/main.tf
+++ aws-terraform-infra/envs/dev/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "./modules/vpc"
+  source = "../../modules/vpc"
 
     vpc_cidr = "10.0.0.0/16"
     name = "dsoapp-dev"
@@ -14,7 +14,7 @@ module "vpc" {
 }
 
 module "security" {
-  source = "./modules/security"
+  source = "../../modules/security"
 
     name = "dsoapp-dev"
     vpc_id = module.vpc.vpc_id
@@ -26,7 +26,7 @@ module "security" {
 }
 
 module "alb" {
-  source = "./modules/alb"
+  source = "../../modules/alb"
 
     name = "dsoapp-dev"
     vpc_id = module.vpc.vpc_id
@@ -39,7 +39,7 @@ module "alb" {
 }
 
 module "asg" {
-  source = "./modules/asg"
+  source = "../../modules/asg"
 
     name = "dsoapp-dev"
     ami_id = data.aws_ami.amazon_linux.id
@@ -55,7 +55,7 @@ module "asg" {
 }
 
 module "iam" {
-  source = "./modules/iam"
+  source = "../../modules/iam"
 
     name = "dsoapp-dev"
     tags = {
@@ -65,7 +65,7 @@ module "iam" {
 }
 
 module "bastion" {
-  source = "./modules/bastion"
+  source = "../../modules/bastion"
 
     name = "dsoapp-dev"
     ami_id = data.aws_ami.amazon_linux.id
